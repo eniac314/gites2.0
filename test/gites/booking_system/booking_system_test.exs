99f136d598442a29484c167fd4e99be3d7bfd0f3
fayong <eniac314@gmail.com>
2019-03-04 21:46:49 +0100

    fixed bug in tests

--- test/gites/booking_system/booking_system_test.exs
+++ test/gites/booking_system/booking_system_test.exs
@@ -6,9 +6,9 @@ defmodule Gites.BookingSystemTest do
   describe "bookings" do
     alias Gites.BookingSystem.Booking
 
-    @valid_attrs %{add_address: "some add_address", address: "some address", check_in: 42, check_out: 42, city: "some city", confirmed: true, country: "some country", email: "some email", first_name: "some first_name", last_name: "some last_name", nbr_adults: 42, nbr_children: 42, phone1: "some phone1", phone2: "some phone2", postcode: 42, title: "some title"}
-    @update_attrs %{add_address: "some updated add_address", address: "some updated address", check_in: 43, check_out: 43, city: "some updated city", confirmed: false, country: "some updated country", email: "some updated email", first_name: "some updated first_name", last_name: "some updated last_name", nbr_adults: 43, nbr_children: 43, phone1: "some updated phone1", phone2: "some updated phone2", postcode: 43, title: "some updated title"}
-    @invalid_attrs %{add_address: nil, address: nil, check_in: nil, check_out: nil, city: nil, confirmed: nil, country: nil, email: nil, first_name: nil, last_name: nil, nbr_adults: nil, nbr_children: nil, phone1: nil, phone2: nil, postcode: nil, title: nil}
+    @valid_attrs %{add_address: "some add_address", address: "some address", check_in: 42, check_out: 42, city: "some city", comments: "some comments", confirmed: true, country: "some country", email: "some email", first_name: "some first_name", last_name: "some last_name", nbr_adults: 42, nbr_children: 42, phone1: "some phone1", phone2: "some phone2", postcode: 42, title: "some title"}
+    @update_attrs %{add_address: "some updated add_address", address: "some updated address", check_in: 43, check_out: 43, city: "some updated city", comments: "some updated comments", confirmed: false, country: "some updated country", email: "some updated email", first_name: "some updated first_name", last_name: "some updated last_name", nbr_adults: 43, nbr_children: 43, phone1: "some updated phone1", phone2: "some updated phone2", postcode: 43, title: "some updated title"}
+    @invalid_attrs %{add_address: nil, address: nil, check_in: nil, check_out: nil, city: nil, comments: nil, confirmed: nil, country: nil, email: nil, first_name: nil, last_name: nil, nbr_adults: nil, nbr_children: nil, phone1: nil, phone2: nil, postcode: nil, title: nil}
 
     def booking_fixture(attrs \\ %{}) do
       {:ok, booking} =
@@ -36,6 +36,7 @@ defmodule Gites.BookingSystemTest do
       assert booking.check_in == 42
       assert booking.check_out == 42
       assert booking.city == "some city"
+      assert booking.comments == "some comments"
       assert booking.confirmed == true
       assert booking.country == "some country"
       assert booking.email == "some email"
@@ -61,6 +62,7 @@ defmodule Gites.BookingSystemTest do
       assert booking.check_in == 43
       assert booking.check_out == 43
       assert booking.city == "some updated city"
+      assert booking.comments == "some updated comments"
       assert booking.confirmed == false
       assert booking.country == "some updated country"
       assert booking.email == "some updated email"
@@ -153,94 +155,5 @@ defmodule Gites.BookingSystemTest do
     end
   end
 
-  describe "bookings" do
-    alias Gites.BookingSystem.Booking
-
-    @valid_attrs %{add_address: "some add_address", address: "some address", check_in: 42, check_out: 42, city: "some city", comments: "some comments", confirmed: true, country: "some country", email: "some email", first_name: "some first_name", last_name: "some last_name", nbr_adults: 42, nbr_children: 42, phone1: "some phone1", phone2: "some phone2", postcode: 42, title: "some title"}
-    @update_attrs %{add_address: "some updated add_address", address: "some updated address", check_in: 43, check_out: 43, city: "some updated city", comments: "some updated comments", confirmed: false, country: "some updated country", email: "some updated email", first_name: "some updated first_name", last_name: "some updated last_name", nbr_adults: 43, nbr_children: 43, phone1: "some updated phone1", phone2: "some updated phone2", postcode: 43, title: "some updated title"}
-    @invalid_attrs %{add_address: nil, address: nil, check_in: nil, check_out: nil, city: nil, comments: nil, confirmed: nil, country: nil, email: nil, first_name: nil, last_name: nil, nbr_adults: nil, nbr_children: nil, phone1: nil, phone2: nil, postcode: nil, title: nil}
-
-    def booking_fixture(attrs \\ %{}) do
-      {:ok, booking} =
-        attrs
-        |> Enum.into(@valid_attrs)
-        |> BookingSystem.create_booking()
-
-      booking
-    end
-
-    test "list_bookings/0 returns all bookings" do
-      booking = booking_fixture()
-      assert BookingSystem.list_bookings() == [booking]
-    end
-
-    test "get_booking!/1 returns the booking with given id" do
-      booking = booking_fixture()
-      assert BookingSystem.get_booking!(booking.id) == booking
-    end
-
-    test "create_booking/1 with valid data creates a booking" do
-      assert {:ok, %Booking{} = booking} = BookingSystem.create_booking(@valid_attrs)
-      assert booking.add_address == "some add_address"
-      assert booking.address == "some address"
-      assert booking.check_in == 42
-      assert booking.check_out == 42
-      assert booking.city == "some city"
-      assert booking.comments == "some comments"
-      assert booking.confirmed == true
-      assert booking.country == "some country"
-      assert booking.email == "some email"
-      assert booking.first_name == "some first_name"
-      assert booking.last_name == "some last_name"
-      assert booking.nbr_adults == 42
-      assert booking.nbr_children == 42
-      assert booking.phone1 == "some phone1"
-      assert booking.phone2 == "some phone2"
-      assert booking.postcode == 42
-      assert booking.title == "some title"
-    end
-
-    test "create_booking/1 with invalid data returns error changeset" do
-      assert {:error, %Ecto.Changeset{}} = BookingSystem.create_booking(@invalid_attrs)
-    end
-
-    test "update_booking/2 with valid data updates the booking" do
-      booking = booking_fixture()
-      assert {:ok, %Booking{} = booking} = BookingSystem.update_booking(booking, @update_attrs)
-      assert booking.add_address == "some updated add_address"
-      assert booking.address == "some updated address"
-      assert booking.check_in == 43
-      assert booking.check_out == 43
-      assert booking.city == "some updated city"
-      assert booking.comments == "some updated comments"
-      assert booking.confirmed == false
-      assert booking.country == "some updated country"
-      assert booking.email == "some updated email"
-      assert booking.first_name == "some updated first_name"
-      assert booking.last_name == "some updated last_name"
-      assert booking.nbr_adults == 43
-      assert booking.nbr_children == 43
-      assert booking.phone1 == "some updated phone1"
-      assert booking.phone2 == "some updated phone2"
-      assert booking.postcode == 43
-      assert booking.title == "some updated title"
-    end
-
-    test "update_booking/2 with invalid data returns error changeset" do
-      booking = booking_fixture()
-      assert {:error, %Ecto.Changeset{}} = BookingSystem.update_booking(booking, @invalid_attrs)
-      assert booking == BookingSystem.get_booking!(booking.id)
-    end
-
-    test "delete_booking/1 deletes the booking" do
-      booking = booking_fixture()
-      assert {:ok, %Booking{}} = BookingSystem.delete_booking(booking)
-      assert_raise Ecto.NoResultsError, fn -> BookingSystem.get_booking!(booking.id) end
-    end
-
-    test "change_booking/1 returns a booking changeset" do
-      booking = booking_fixture()
-      assert %Ecto.Changeset{} = BookingSystem.change_booking(booking)
-    end
-  end
+  
 end
