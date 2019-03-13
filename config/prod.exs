f7028745f8ae70deb55d32b1ef0be1fd973e1024
fayong <eniac314@gmail.com>
2019-03-13 17:56:18 +0100

    --

--- config/prod.exs
+++ config/prod.exs
@@ -17,7 +17,7 @@ config :gites, GitesWeb.Endpoint,
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
 # Database configuration
-config :platform, Platform.Repo,
+config :gites, Gites.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
